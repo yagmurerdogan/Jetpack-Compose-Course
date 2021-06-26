ef580689a42e1439422218c32128667c6d0b476c
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-06-26 17:31:38 +0300

    Create example for inheritance

--- SimpleTextComposeApplication/app/src/main/java/com/yagmurerdogan/simpletextcomposeapplication/Example.kt
+++ SimpleTextComposeApplication/app/src/main/java/com/yagmurerdogan/simpletextcomposeapplication/Example.kt
@@ -4,7 +4,7 @@ package com.yagmurerdogan.simpletextcomposeapplication
         This file for Kotlin basics
  */
 
-public final class Person constructor(private val age: Int) {
+open class Person constructor(val age: Int) {
 
     open fun talk() {
         val message: String = "I am simple person aged $age."
@@ -12,7 +12,28 @@ public final class Person constructor(private val age: Int) {
     }
 }
 
+class Teacher constructor(age: Int): Person(age) {
+
+    override fun talk() {
+        val message: String = "I am a teacher aged $age."
+        println(message)
+    }
+
+    fun teach() {
+        println("I teach.")
+    }
+}
+
 fun main(args: Array<String>) {
+
     val simplePerson: Person = Person(20)
     simplePerson.talk()
+
+    var teacher = Teacher(age = 30)
+    teacher.talk()
+    teacher.teach()
+
+    teacher = Teacher(age = 40)
+    teacher.talk()
+
 }
