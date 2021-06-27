c6441d2cb6b02813b10293c30b6c110eab7270eb
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-06-27 16:14:12 +0300

    Use text and button composables

--- SimpleTextComposeApplication/app/src/main/java/com/yagmurerdogan/simpletextcomposeapplication/MainActivity.kt
+++ SimpleTextComposeApplication/app/src/main/java/com/yagmurerdogan/simpletextcomposeapplication/MainActivity.kt
@@ -1,38 +1,56 @@
 package com.yagmurerdogan.simpletextcomposeapplication
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCompositionContext
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.yagmurerdogan.simpletextcomposeapplication.ui.theme.SimpleTextComposeApplicationTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /*
+            setContent(content = {
+            Greeting(name = "Thanks for lambda functions")
+        })
+
+        ~ Notice that composable function names are starting with upper case
+
+         */
         setContent {
-            SimpleTextComposeApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            GreetingButton()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
+fun GreetingText(name: String) {
     Text(text = "Hello $name!")
+}
+
+@Composable
+fun GreetingButton() {
+    val context = LocalContext.current
+    Button(onClick = {
+        Toast.makeText(context, "You clicked me!", Toast.LENGTH_SHORT).show()
+    }) {
+        GreetingText(name = "button")
+        GreetingText(name = "remaining button")
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    SimpleTextComposeApplicationTheme {
-        Greeting("Android")
-    }
+    GreetingButton()
 }
