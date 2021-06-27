6f28198d9951ea8ee5b273c21acf99abe1551e1b
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-06-27 17:14:35 +0300

    Add clickable and padding

--- SimpleTextComposeApplication/app/src/main/java/com/yagmurerdogan/simpletextcomposeapplication/MainActivity.kt
+++ SimpleTextComposeApplication/app/src/main/java/com/yagmurerdogan/simpletextcomposeapplication/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.yagmurerdogan.simpletextcomposeapplication
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCompositionContext
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.yagmurerdogan.simpletextcomposeapplication.ui.theme.SimpleTextComposeApplicationTheme
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,10 +35,17 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun GreetingText(name: String) {
+    val context = LocalContext.current
     Text(
         text = "Hello $name!",
         modifier = Modifier
-            .size(width = 80.dp, height = 240.dp)
+            .size(width = 200.dp, height = 240.dp)
+            .padding(24.dp)
+            .clickable(onClick = {
+                Toast
+                    .makeText(context, "Text Clicked!", Toast.LENGTH_SHORT)
+                    .show()
+            })
             //.fillMaxWidth(0.5f)
             //.fillMaxHeight()
             //.fillMaxSize()
@@ -51,7 +56,9 @@ fun GreetingText(name: String) {
 fun GreetingButton() {
     val context = LocalContext.current
     Button(onClick = {
-        Toast.makeText(context, "You clicked me!", Toast.LENGTH_SHORT).show()
+        Toast
+            .makeText(context, "Button Clicked!", Toast.LENGTH_SHORT)
+            .show()
     }) {
         GreetingText(name = "button")
         GreetingText(name = "remaining button")
