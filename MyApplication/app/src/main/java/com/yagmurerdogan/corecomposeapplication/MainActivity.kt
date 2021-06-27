9d2da73b35fc851da68c0d1dab42a14ec7c1d751
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-06-27 20:42:06 +0300

    Create example for nesting row with columns

--- MyApplication/app/src/main/java/com/yagmurerdogan/corecomposeapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/yagmurerdogan/corecomposeapplication/MainActivity.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -35,22 +32,27 @@ fun MainScreen() {
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            HorizontalColoredBar(color = Color.Yellow)
-            HorizontalColoredBar(color = Color.Green)
-            HorizontalColoredBar(color = Color.Blue)
-            HorizontalColoredBar(color = Color.LightGray)
-            HorizontalColoredBar(color = Color.Black)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                ColoredSquare(color = Color.Yellow)
+                ColoredSquare(color = Color.Green)
+            }
+            ColoredSquare(color = Color.Blue)
+            ColoredSquare(color = Color.LightGray)
+            ColoredSquare(color = Color.Black)
         }
     }
 }
 
 @Composable
-fun HorizontalColoredBar(color: Color) {
+fun ColoredSquare(color: Color) {
     Surface(
         color = color,
         modifier = Modifier
             .height(100.dp)
-            .width(350.dp)
+            .width(100.dp)
     ) { }
 }
 
