b31a3751a7b97bd67a7388e748442417e33821be
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-06-28 14:47:35 +0300

    Create profile icon for card

--- ProfileCardLayout/app/src/main/java/com/yagmurerdogan/profilecardlayout/MainActivity.kt
+++ ProfileCardLayout/app/src/main/java/com/yagmurerdogan/profilecardlayout/MainActivity.kt
@@ -3,25 +3,28 @@ package com.yagmurerdogan.profilecardlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.yagmurerdogan.profilecardlayout.ui.theme.ProfileCardLayoutTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                    MainScreen("Android")
+            MainScreen("Android")
         }
     }
 }
@@ -39,11 +42,16 @@ fun MainScreen(name: String) {
 @Composable
 fun ProfileCard() {
     Card(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top),
         elevation = 8.dp
     ) {
         Row(
-            modifier = Modifier.wrapContentSize()
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
         ) {
             ProfilePicture()
             ProfileContent()
@@ -53,16 +61,30 @@ fun ProfileCard() {
 
 @Composable
 fun ProfilePicture() {
-
+    Card(
+        shape = CircleShape,
+        border = BorderStroke(width = 2.dp, color = Color.Black),
+        modifier = Modifier.padding(16.dp),
+        elevation = 4.dp
+    ) {
+        Image(
+            painter = painterResource(
+                id = R.drawable.dracula
+            ),
+            contentDescription = "Profile image",
+            modifier = Modifier.size(72.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
 }
 
 @Composable
 fun ProfileContent() {
-
+    Text(text = "Dracula")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-        MainScreen("Android")
+    MainScreen("Android")
 }
