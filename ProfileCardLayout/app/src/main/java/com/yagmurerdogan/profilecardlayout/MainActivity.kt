59d24ded2b028f67c02511c69237800080831e43
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-06-28 22:25:04 +0300

    Use coil library for loading images

--- ProfileCardLayout/app/src/main/java/com/yagmurerdogan/profilecardlayout/MainActivity.kt
+++ ProfileCardLayout/app/src/main/java/com/yagmurerdogan/profilecardlayout/MainActivity.kt
@@ -22,8 +22,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.transform.CircleCropTransformation
+import com.google.accompanist.coil.rememberCoilPainter
 import com.yagmurerdogan.profilecardlayout.ui.theme.LightGreen
 import com.yagmurerdogan.profilecardlayout.ui.theme.MyTheme
 
@@ -47,7 +50,7 @@ fun MainScreen(userProfiles: List<UserProfile> = userProfileList) {
             modifier = Modifier.fillMaxSize()
         ) {
             LazyColumn {
-                items(userProfiles){ userProfile ->
+                items(userProfiles) { userProfile ->
                     ProfileCard(userProfile = userProfile)
                 }
             }
@@ -104,12 +107,14 @@ fun ProfilePicture(drawableId: Int, onlineStatus: Boolean) {
         elevation = 4.dp
     ) {
         Image(
-            painter = painterResource(
-                id = drawableId
+            painter = rememberCoilPainter(
+                request = drawableId,
+                requestBuilder = {
+                    transformations(CircleCropTransformation())
+                },
             ),
-            contentDescription = "Profile image",
             modifier = Modifier.size(72.dp),
-            contentScale = ContentScale.Crop
+            contentDescription = "Profile Picture"
         )
     }
 }
