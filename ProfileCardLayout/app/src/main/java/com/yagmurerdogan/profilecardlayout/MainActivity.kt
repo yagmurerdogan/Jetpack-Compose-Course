49a4caf579ae5f8a780b567a328c6d59b8c32bbc
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-06-28 14:59:10 +0300

    Create text content for profile card

--- ProfileCardLayout/app/src/main/java/com/yagmurerdogan/profilecardlayout/MainActivity.kt
+++ ProfileCardLayout/app/src/main/java/com/yagmurerdogan/profilecardlayout/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -80,7 +78,22 @@ fun ProfilePicture() {
 
 @Composable
 fun ProfileContent() {
-    Text(text = "Dracula")
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "Dracula",
+            style = MaterialTheme.typography.h5
+        )
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) { //for transparency
+            Text(
+                text = "Active now",
+                style = MaterialTheme.typography.body2
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
