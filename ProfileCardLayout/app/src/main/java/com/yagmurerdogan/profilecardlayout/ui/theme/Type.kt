c704b013f16d2d027c6fb3aeae3098d984a4e433
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-06-28 16:39:41 +0300

    Customize font type in theme

--- ProfileCardLayout/app/src/main/java/com/yagmurerdogan/profilecardlayout/ui/theme/Type.kt
+++ ProfileCardLayout/app/src/main/java/com/yagmurerdogan/profilecardlayout/ui/theme/Type.kt
@@ -12,6 +12,11 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    ),
+    h5 = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 22.sp,
+        letterSpacing = 1.sp
     )
     /* Other default text styles to override
     button = TextStyle(
