ec5a908ca38cf3a362d2bc5122085807e52fe483
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-28 09:55:24 +0300

    Create a file for lesson notes

---
+++ MealzApp/app/src/main/java/com/yagmurerdogan/mealzapp/ModukleNotes.kt
@@ -0,0 +1,48 @@
+package com.yagmurerdogan.mealzapp
+
+/*
+    --- Introduction to MVC, MVP and MVVM ---
+
+    Presentation patterns are design patterns,
+     and they are here to help us in developing
+     good applications  and scalable projects
+     that have decoupled components.
+
+    --- MVC
+
+    View(xml) -> Controller Activity ->Model
+
+    Disadvantages:
+
+    Your code is highly coupled - no separation of concerns
+    Difficult to test
+    The project is difficult to maintain
+    Little scalability
+
+    --- MVP
+
+    View(Activity/Fragment) <-> Presenter -> Model
+
+    --- MVVM
+
+    View(Activity/Fragment) -> ViewModel -> Model
+
+    Advantages:
+
+    Your code is decoupled - separation of concerns
+    Code is easily testable
+    The package structure is even easier to navigate
+    THe project is even easier to maintain
+    Scalable - your team can add new features even more
+    quickly
+
+    -> ViewModel is a class that is responsible for
+    preparing and managing the state of the UI for
+    Fragments or Activities, or composable.
+
+    
+
+
+
+
+ */
