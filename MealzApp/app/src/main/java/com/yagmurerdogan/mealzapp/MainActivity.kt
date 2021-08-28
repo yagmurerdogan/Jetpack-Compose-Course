e8abf65bee5b5729927b6d4b68ce9e4238ae9b6f
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-28 09:22:59 +0300

    Create Mealz App in module nine

---
+++ MealzApp/app/src/main/java/com/yagmurerdogan/mealzapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.yagmurerdogan.mealzapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.yagmurerdogan.mealzapp.ui.theme.MealzAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MealzAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MealzAppTheme {
+        Greeting("Android")
+    }
+}
