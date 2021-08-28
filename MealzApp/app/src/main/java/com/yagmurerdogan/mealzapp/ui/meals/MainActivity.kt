2a176600a67486e8bd931cca8f9003e7dce9e01e
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-28 10:24:54 +0300

    Refactor main activity

--- MealzApp/app/src/main/java/com/yagmurerdogan/mealzapp/ui/meals/MainActivity.kt
+++ MealzApp/app/src/main/java/com/yagmurerdogan/mealzapp/ui/meals/MainActivity.kt
@@ -1,13 +1,12 @@
-package com.yagmurerdogan.mealzapp
+package com.yagmurerdogan.mealzapp.ui.meals
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.yagmurerdogan.mealzapp.ui.theme.MealzAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +14,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MealzAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                    MealsCategoriesScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MealsCategoriesScreen() {
+    val viewModel: MealsCategoriesViewModel = viewModel()
+    Text(text = "Hello Compose")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MealzAppTheme {
-        Greeting("Android")
+        MealsCategoriesScreen()
     }
 }
